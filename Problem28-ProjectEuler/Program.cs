73709e76765c5b6d0ae10b7a73e418861a2a7f48
nikil <nikil@DESKTOP-73MHGNO>
2022-01-10 20:42:55 -0500

    Add project files.

---
+++ Problem28-ProjectEuler/Program.cs
@@ -0,0 +1,25 @@
+﻿int topLimit = 1001*1001;
+int sum = 1;
+int counter = 0;
+int spaces = 1 + 1;
+int circle = 0;
+
+for(int i = 2; i <= topLimit; i++)
+{
+    circle++;
+    if(circle == spaces)
+    {
+        sum = sum + i;
+        counter++;
+        circle = 0;
+    }
+
+    if(counter == 4)
+    {
+        spaces = spaces + 2;
+
+        counter = 0;
+    }
+}
+
+Console.WriteLine(sum);
